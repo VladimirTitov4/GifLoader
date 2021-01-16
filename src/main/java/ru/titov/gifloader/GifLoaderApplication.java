ce86c1edbad9669b7cb8cd3441444a9c42dfe51e
Vladimir Titov <vtitov4@yandex.ru>
2021-01-16 23:57:37 +0300

    initial commit

---
+++ src/main/java/ru/titov/gifloader/GifLoaderApplication.java
@@ -0,0 +1,13 @@
+package ru.titov.gifloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GifLoaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/titov/gifloader/GifloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.titov.gifloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifloaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
